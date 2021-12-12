d62400ab59508eaea86a1d0d2c8ca2adf2223c49
yahomi-dev <yahomi.dev@gmail.com>
2021-12-12 12:42:11 +0900

    fix callname

--- src/CardNumber.java
+++ src/CardNumber.java
@@ -1,17 +1,17 @@
 public enum CardNumber {
-    ACE   (1),
-    DEUCE (2),
-    TREY  (3),
-    CATER (4),
-    CINQU (5),
-    SICE  (6),
-    SEVEN (7),
-    EIGHT (8),
-    NINE  (9),
-    TEN   (10),
-    JACK  (11),
-    QUEEN (12),
-    KING  (13);
+    ACE    (1),
+    DEUCE  (2),
+    TREY   (3),
+    CATER  (4),
+    CINQUE (5),
+    SICE   (6),
+    SEVEN  (7),
+    EIGHT  (8),
+    NINE   (9),
+    TEN    (10),
+    JACK   (11),
+    QUEEN  (12),
+    KING   (13);
 
     private int cardNumber;
 
